f11c023b427f38fe3d30c9bec536f832396ca62c
J0S3R0DR1GU3Z2122 <josemanuelisabella22@gmail.com>
2025-12-07 21:52:17 -0400

    Agregar integrante - Manuel Rodríguez 31657231

--- proyecto.c
+++ proyecto.c
@@ -3,7 +3,8 @@ int main() {
     printf("Integrantes del grupo\n");
     printf("Ronald Rodriguez C.I: 31.762.090\n");
     printf("Alexander Rodriguez C.I: 31.797.479\n");
-    printf("Alexander Sánchez C.I:31.644.118\n");
-    printf("David Torres C.I:31.762.289\n");
+    printf("Alexander Sánchez C.I: 31.644.118\n");
+    printf("David Torres C.I: 31.762.289\n");
+    printf("Manuel Rodríguez C.I: 31.657.231\n");
     return 0;
 }
